f554df3304885055326ea68a91e51fb33fdfa12f
wolfgruber-htl <129189640+wolfgruber-htl@users.noreply.github.com>
2025-11-27 12:45:39 +0100

    Hey there I`m using Whatsapp

---
+++ prjct_school-library/prjct_school-library/src/main/java/htlle/PrjctSchoolLibraryApplication.java
@@ -0,0 +1,13 @@
+package htlle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjctSchoolLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjctSchoolLibraryApplication.class, args);
+	}
+
+}
---
+++ prjct_school-library/prjct_school-library/src/test/java/htlle/PrjctSchoolLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package htlle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjctSchoolLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
